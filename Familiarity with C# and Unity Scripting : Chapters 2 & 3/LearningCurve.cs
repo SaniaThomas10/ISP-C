df6dc84dd77d028c2f3556bbcf5906b460dc3858
SaniaThomas10 <s.thomas26@ncf.edu>
2026-01-08 11:40:03 -0500

    Part 3 of assignment

---
+++ Familiarity with C# and Unity Scripting : Chapters 2 & 3/LearningCurve.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    public void GenerateCharacter()
+    {
+        Debug.Log("Brook Davis");
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateCharacter();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
